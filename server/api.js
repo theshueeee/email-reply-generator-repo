5435b282d496ff267564633fb8e13c5634692069
Theshueraj <theshueraj.gunalan@gmail.com>
2025-03-13 02:30:09 +0800

    set up backend api

---
+++ server/api.js
@@ -0,0 +1,21 @@
+import {HfInference} from "@huggingface/inference";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const HF_ACCESS_TOKEN = process.env.HUGGINGFACE_TOKEN;
+const inference = new HfInference(HF_ACCESS_TOKEN);
+const model = "deepseek-ai/DeepSeek-R1";
+
+
+if (!HF_ACCESS_TOKEN){
+  console.log("API key not set up")
+  process.exit(1)
+}
+
+export default inference;
+
+
+
+
+
